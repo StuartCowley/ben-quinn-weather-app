143ba5c73b9231dd03fdc7e5b16c4fb4dc940cca
Ben Quinn <benjamesquinn@gmail.com>
2022-05-30 17:55:33 +0100

    Added ForecastSummaries component

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -6,21 +6,22 @@ function ForecastSummary({ date, description, icon, temperature }) {
     <div className="forecast-summary">
       <div className="forecast-summary_date">{date}</div>
       <div className="forecast-summary_icon">{icon}</div>
-      <div className="forecast-summary_temperature">{temperature.max}@deg;c</div>
+      <div className="forecast-summary_temperature">
+        {temperature.max}&deg;c
+      </div>
       <div className="forecast-summary_description">{description}</div>
     </div>
   );
 }
 
 ForecastSummary.propTypes = {
-    date: PropTypes.number.isRequired,
-    description: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    temperature: PropTypes.shape({
-      min: PropTypes.number,
-      max: PropTypes.number,
-    }).isRequired,
-  };
+  date: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  temperature: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }).isRequired,
 };
 
 export default ForecastSummary;
