c94371b0efe196dd10db2601f6b8e487794d8111
Ben Quinn <benjamesquinn@gmail.com>
2022-06-04 14:57:26 +0100

    Updated tests for external data

--- src/tests/ForecastSummary.test.js
+++ src/tests/ForecastSummary.test.js
@@ -21,6 +21,7 @@ describe("ForecastSummary", () => {
         description={validProps.description}
         icon={validProps.icon}
         temperature={validProps.temperature}
+        onSelect={validProps.onSelect}
       />
     );
 
@@ -34,6 +35,7 @@ describe("ForecastSummary", () => {
         description={validProps.description}
         icon={validProps.icon}
         temperature={validProps.temperature}
+        onSelect={validProps.onSelect}
       />
     );
 
